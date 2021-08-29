fa21e86b901669eace34305a351dbfa7139f2023
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-08-29 12:04:50 -0300

    add Routes para status

--- src/main/java/br/com/bode/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/bode/mvc/mudi/controller/HomeController.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.bode.mvc.mudi.model.Pedido;
+import br.com.bode.mvc.mudi.model.StatusPedido;
 import br.com.bode.mvc.mudi.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 	
 	private PedidoRepository pedidoRepository;
@@ -19,14 +24,26 @@ public class HomeController {
 		this.pedidoRepository = pedidoRepository;
 	}
 	
-	
-
-	@GetMapping("/home")
+	@GetMapping
 	public ModelAndView home(Model model) {
 		List<Pedido> pedidos = pedidoRepository.findAll();
 		ModelAndView mv = new ModelAndView("home");
 		mv.addObject("pedidos", pedidos);
 		return mv;
+	}
+	
+	@GetMapping("/{status}")
+	public ModelAndView porStatus(@PathVariable("status") String status, Model model) {
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("pedidos", pedidos);
+		mv.addObject("status", status);
+		return mv;
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
 	}
 	
 }
